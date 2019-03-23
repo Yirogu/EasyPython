5f6af1838446dcb5d8b943ec9b0026f20334333f
Yirogu <stachudcs@gmail.com>
2019-03-23 15:35:24 +0100

    add geometry

--- TKINTER/window.py
+++ TKINTER/window.py
@@ -1,14 +1,19 @@
 from tkinter import*
 class Window(Frame) :
+
     def __init__(self,master = None) :
         Frame.__init__(self,master)
         self.master = master
         self.init_window()
+
     def init_window(self):
         self.master.title("Gui")
         self.pack(fill=BOTH,expand=1)
-        quitButton = Button(self,text = "Quit")
+        quitButton = Button(self,text = "Quit",command = self.client_exit)
         quitButton.place(x=0,y=0)
+
+    def client_exit (self):
+        exit()
 
 root = Tk()
 root.geometry("400x300")
