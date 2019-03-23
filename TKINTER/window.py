3bbdad6ee4b8dce7acca519c7e373848bbbe20a6
Yirogu <stachudcs@gmail.com>
2019-03-23 16:18:12 +0100

    menu show image and show text

--- TKINTER/window.py
+++ TKINTER/window.py
@@ -1,4 +1,5 @@
 from tkinter import*
+from PIL import Image,ImageTk
 class Window(Frame) :
 
     def __init__(self,master = None) :
@@ -13,16 +14,33 @@ class Window(Frame) :
         self.master.config(menu=menu)
 
         file = Menu(menu)
+        file.add_command(label = 'Save')
         file.add_command(label = 'Exit',command = self.client_exit)
         menu.add_cascade(label = 'File',menu = file)
 
         edit = Menu(menu)
-        edit.add_command(label='Undo')
-        menu.add_cascade(label = 'Edit' menu = edit)
+        edit.add_command(label='Show Image',command=self.showImg)
+        edit.add_command(label='Show Text',command=self.showTxt)
+        menu.add_cascade(label = 'Edit', menu = edit)
 
 
     def client_exit (self):
         exit()
+
+
+    def showImg(self) :
+        load =Image.open('Images/pick.jpg')
+        render = ImageTk.PhotoImage(load)
+
+        img = Label(self,image=render)
+        img.image = render
+        img.place(x=0,y=0)
+
+    def showTxt(self):
+        text = Label(self,text = "hey there good looking")
+        text.pack()
+
+
 
 root = Tk()
 root.geometry("400x300")
