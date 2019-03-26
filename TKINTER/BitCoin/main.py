623fb64c896062e3ca96b323f28b04206d9495cd
Yirogu <stachudcs@gmail.com>
2019-03-26 20:47:54 +0100

    part 6 add graf and tools from matplotlib

--- TKINTER/BitCoin/main.py
+++ TKINTER/BitCoin/main.py
@@ -1,3 +1,8 @@
+import matplotlib
+matplotlib.use("TkAgg")
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg,NavigationToolbar2Tk
+from matplotlib.figure import Figure
 import tkinter as tk
 from tkinter import ttk
 
@@ -7,21 +12,19 @@ class SeaofBTCapp(tk.Tk) :
     def __init__ (self,*args,**kwargs) :
 
         tk.Tk.__init__(self,*args,**kwargs)
+
         tk.Tk.title(self,"BitCoin")
-        container = tk.Frame(self)
         img = tk.PhotoImage(file='icon.gif')
-        # tk.Tk.wm_iconbitmap(self,'@icon.gif')
         self.tk.call('wm','iconphoto',self._w,img)
 
-
+        container = tk.Frame(self)
         container.pack(side='top',fill='both', expand = True )
-
         container.grid_rowconfigure(0,weight =1)
         container.grid_columnconfigure(0,weight = 1)
 
         self.frames = {}
 
-        for F in (StartPage,PageOne,PageTwo) :
+        for F in (StartPage,PageOne,PageTwo,PageThree) :
 
             frame  = F(container,self)
 
@@ -52,9 +55,9 @@ class StartPage(tk.Frame) :
         command = lambda :controller.show_frame(PageTwo))
         button2.pack()
         #
-        # button3 = tk.Button(self,text = "Visit page1",
-        # command = lambda :controller.show_frame(PageOne))
-        # button3.pack()
+        button3 = tk.Button(self,text = "Graph Page",
+        command = lambda :controller.show_frame(PageThree))
+        button3.pack()
 
 class PageOne(tk.Frame) :
     def __init__(self,parent,controller):
@@ -84,7 +87,26 @@ class PageTwo(tk.Frame) :
         command = lambda :controller.show_frame(StartPage))
         button2.pack()
 
+class PageThree(tk.Frame) :
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self,text="Graph Page",font =LARGE_FONT)
+        label.pack(pady=10,padx=10)
 
+        button1 = ttk.Button(self,text = "Back to Home",
+        command = lambda :controller.show_frame(StartPage))
+        button1.pack()
+
+        f = Figure(figsize = (5,5), dpi =100)
+        a = f.add_subplot(111)
+        a.plot([1,2,3,4,5,6,7,8],[5,6,1,4,8,9,3,5])
+
+        canvas = FigureCanvasTkAgg(f,self)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side = tk.TOP,fill = tk.BOTH,expand = True)
+        toolbar = NavigationToolbar2Tk(canvas,self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side = tk.TOP,fill = tk.BOTH,expand = True)
 
 
 
