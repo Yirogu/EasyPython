15e8d28e4ecfe90c164ac77fc52f359e0bc47b30
Yirogu <stachudcs@gmail.com>
2019-04-03 21:20:56 +0200

    Add new file to folder Class

---
+++ Class/pet.py
@@ -0,0 +1,10 @@
+class Pet :
+    def __init__ (self,name,age,rasa) :
+        self.age = age
+        self.name = name
+        self.rasa = rasa
+        self.names = print(f"the name of dog is {name}")
+
+
+tofik = Pet("Tofik",21,"Bullterier")
+tofik.names
