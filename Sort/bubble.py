88e09ffd21a23b549c39a597be58367095ab203c
Yirogu <stachudcs@gmail.com>
2019-04-09 21:18:00 +0200

    bubble sort

--- Sort/bubble.py
+++ Sort/bubble.py
@@ -1,19 +1,14 @@
 numbers =[3,2,4,3,1,2,0]
-tmp = numbers[0]
-inUse = 0
-
-for num in range(len(numbers)) :
-    inUse = numbers[num]
-    if tmp > inUse :
-        numbers[num] = tmp
-        numbers[num-1] = inUse
-    else :
-        tmp = inUse
-    print(numbers)
 
 
-
-
-
-
-print(numbers)
+for num in range(len(numbers)):
+    print(f"step {num +1} result {numbers}")
+    tmp = numbers[0]
+    inUse = 0
+    for num in range(len(numbers)) :
+        inUse = numbers[num]
+        if tmp > inUse :
+            numbers[num] = tmp
+            numbers[num-1] = inUse
+        else :
+            tmp = inUse
