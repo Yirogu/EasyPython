74df35515bddc61fce9bdcb7ef5f4fd7d4e8d348
Yirogu <stachudcs@gmail.com>
2019-04-10 23:20:38 +0200

    the first step in the algorithm sort  merge

--- Sort/merge.py
+++ Sort/merge.py
@@ -1,4 +1,4 @@
-numbers = [5,7,6,1,9,2,8,4,3]
+numbers = [5,7,6,1,9,2,8,4,5]
 diveNums = []
 total = []
 
@@ -8,16 +8,27 @@ while x == True :
     i = i+1
     if len(numbers)%2 == 0 :
         for num in range( (int (len(numbers) /2)) ) :
-            diveNums.append(numbers.pop(num))
+            diveNums.append(numbers.pop(0))
+
 
     else :
         for num in range( (int (len(numbers) /2)) + 1 ) :
-            diveNums.append(numbers.pop(num))
+            diveNums.append(numbers.pop(0))
 
     total.append(diveNums)
     diveNums = []
+
+    for num in range(len(numbers)) :
+            diveNums.append(numbers.pop(0))
+
+
+
+    total.append(diveNums)
+    diveNums = []
+
+
+
     # if num+1 == len(numbers) :
     print(i)
     print(total)
-    if len(numbers) == 0 :
-        x = False
+    x = False
