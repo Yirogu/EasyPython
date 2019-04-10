6bae15fb05a91fa1c11ad47366a7a71bc8870941
Yirogu <stachudcs@gmail.com>
2019-04-10 22:40:11 +0200

    add half of first step

--- Sort/merge.py
+++ Sort/merge.py
@@ -1,16 +1,23 @@
 numbers = [5,7,6,1,9,2,8,4,3]
 diveNums = []
+total = []
+
 x = True
 i = 0
 while x == True :
     i = i+1
     if len(numbers)%2 == 0 :
         for num in range( (int (len(numbers) /2)) ) :
-            diveNums[i]
+            diveNums.append(numbers.pop(num))
 
     else :
         for num in range( (int (len(numbers) /2)) + 1 ) :
-            print(num)
+            diveNums.append(numbers.pop(num))
 
+    total.append(diveNums)
+    diveNums = []
     # if num+1 == len(numbers) :
-    x = False
+    print(i)
+    print(total)
+    if len(numbers) == 0 :
+        x = False
