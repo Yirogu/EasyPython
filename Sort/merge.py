16444fdf23d7ef45f7e415a36405ec15e7ccbd81
Yirogu <stachudcs@gmail.com>
2019-04-10 22:28:59 +0200

    dividing numbers into sets, even and odd divisions

--- Sort/merge.py
+++ Sort/merge.py
@@ -1,10 +1,12 @@
 numbers = [5,7,6,1,9,2,8,4,3]
 diveNums = []
 x = True
+i = 0
 while x == True :
+    i = i+1
     if len(numbers)%2 == 0 :
         for num in range( (int (len(numbers) /2)) ) :
-            print(num)
+            diveNums[i]
 
     else :
         for num in range( (int (len(numbers) /2)) + 1 ) :
