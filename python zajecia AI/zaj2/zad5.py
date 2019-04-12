b313e4b9d00e073ee8428fdd21388481682268ff
Yirogu <stachudcs@gmail.com>
2019-04-12 17:45:34 +0200

    best individual

---
+++ python zajecia AI/zaj2/zad5.py
@@ -0,0 +1,18 @@
+from pyeasyga import pyeasyga
+def fitness (individual, data):
+    sum1 = 0
+    sum2 = 0
+    for (selected, (bit,value)) in zip(individual, data):
+        if selected:
+            sum1 = sum1 + value
+        else :
+            sum2 = sum2 + value
+    return sum1-sum2
+
+
+data = [("1",1),("2", 4),("3", 6),("4", 11),("5", 13),("6",20),("7", 35)]
+ga = pyeasyga.GeneticAlgorithm(data)
+
+ga.fitness_function = fitness
+ga.run()
+print (ga.best_individual())
