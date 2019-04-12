5594a02633c6992041e9cf29fd5fcc23439466c2
Yirogu <stachudcs@gmail.com>
2019-04-12 23:11:08 +0200

    zad 3 zaj 3

--- python zajecia AI/zaj3/zad3.py
+++ python zajecia AI/zaj3/zad3.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt 
 from sklearn import datasets
 iris=datasets.load_iris()
 
@@ -54,10 +55,44 @@ from sklearn.naive_bayes import GaussianNB
 
 classifier4=GaussianNB()
 classifier4.fit(x_train,y_train)
+#ewaluacja drzewa na zbiorze testowym
+
 predictions4=classifier4.predict(x_test)
 
 # Multi-layer Perception
-
-redictions4=classifier4.predict(x_test)
 from sklearn.metrics import accuracy_score
 print("Multi-layer Perception",accuracy_score(y_test,predictions4))
+#klasyfikator xxx:  Forests of randomized trees
+
+from sklearn.ensemble import RandomForestClassifier
+#tworzenie drzewa na zbiorze treningowym
+
+classifier5=RandomForestClassifier()
+classifier5.fit(x_train,y_train)
+#ewaluacja drzewa na zbiorze testowym
+
+predictions5=classifier5.predict(x_test)
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+print(" Forests of randomized trees",accuracy_score(y_test,predictions5))
+print(" Forests of randomized trees\n",confusion_matrix(y_test,predictions5))
+
+
+"""
+wykres
+"""
+
+os_x=["Drzewo","somsiad","Naive Bayes","multi-layer",]
+os_y=[accuracy_score(y_test,predictions),accuracy_score(y_test,predictions2),accuracy_score(y_test,predictions3),accuracy_score(y_test,predictions4)]
+plt.bar(os_x, os_y, width=0.1, label='dokladnosc', color='red')
+#plt.bar(ypos+0.2, Printers, width=0.2, label='Printers', color='green')
+#give title
+plt.title('porownanie klasyfikatorow')
+plt.xticks(os_x)
+plt.xlabel('klasyfikatory')
+plt.ylabel('dokladnosc')
+#plt.plot(ypos, Year) will remove the numbers along the y-axis
+#shows legend
+plt.legend()
+#show to show the graph
+plt.show()
