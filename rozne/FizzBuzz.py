abb406ab7ddc1343c7540dd65947a7ff4b39ca1d
Yirogu <stachudcs@gmail.com>
2019-05-08 20:21:17 +0200

    First idea for FizzBuzz

--- rozne/FizzBuzz.py
+++ rozne/FizzBuzz.py
@@ -18,5 +18,27 @@ start = time.clock()
 for number in range(1,100):
     print(f"{number} result is {fizzBuzz(number)}")
 end = time.clock()
-print(end - start)
-#With text
+first = end - start
+#Time  to execute : 0.00030799999999999925,0.0003490000000000021,0.0004289999999999988
+
+#Second Idea without use def
+
+
+
+start2 = time.clock()
+for number in range(1,100):
+    text = ""
+    if number % 3 == 0 :
+        text = "Fizz"
+    if number % 5 == 0 :
+        text += "Buzz"
+    print(f"{number} result is {text}")
+end2 = time.clock()
+second = end2 - start2
+
+#the fastest solution
+if first > second :
+    print(f" the first is faster {first - second} ")
+else :
+    print(f" the second is faster {second - first} ")
+print(first,second)
