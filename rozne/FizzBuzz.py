ba503a453aa06160a28010f4eadde7c40f7889f5
Yirogu <stachudcs@gmail.com>
2019-05-08 20:54:49 +0200

    comparison of methods

--- rozne/FizzBuzz.py
+++ rozne/FizzBuzz.py
@@ -4,7 +4,9 @@
 # Jeśli liczba jest podzielna przez trzy i pięć wypisz “FizzBuzz“
 import time
 
+
 # First Idea
+start = time.clock()
 def fizzBuzz(number):
     text = ""
     if number % 3 == 0 :
@@ -14,11 +16,12 @@ def fizzBuzz(number):
     return text
 
 
-start = time.clock()
-for number in range(1,100):
+
+for number in range(1,101):
     print(f"{number} result is {fizzBuzz(number)}")
 end = time.clock()
-first = end - start
+first = round(end - start,9)
+
 #Time  to execute : 0.00030799999999999925,0.0003490000000000021,0.0004289999999999988
 
 #Second Idea without use def
@@ -26,7 +29,7 @@ first = end - start
 
 
 start2 = time.clock()
-for number in range(1,100):
+for number in range(1,101):
     text = ""
     if number % 3 == 0 :
         text = "Fizz"
@@ -35,10 +38,24 @@ for number in range(1,100):
     print(f"{number} result is {text}")
 end2 = time.clock()
 second = end2 - start2
+second = round(second,9)
 
+#Simple resolution from internet
+start3 = time.clock()
+for num in range(1, 101):
+    string = ""
+    if num % 3 == 0:
+        string = "Fizz"
+    if num % 5 == 0:
+        string = string + "Buzz"
+    if string == "":
+        string = str(num)
+    print(string)
+stop3 = time.clock()
+third = start3 - stop3
 #the fastest solution
-if first > second :
-    print(f" the first is faster {first - second} ")
+if first < third :
+    print(f" the first is faster {third - first } ")
 else :
-    print(f" the second is faster {second - first} ")
-print(first,second)
+    print(f" the third is faster {first - third} ")
+print(first,third)
